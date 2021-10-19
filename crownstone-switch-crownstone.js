abdea9e2af7a244baf8e864e1a38e68a02836cbc
Danielvdd1 <danielvdd1@hotmail.com>
2021-10-19 11:39:16 +0200

    Input field changes depending on if the Crownstone is dimmable.

--- crownstone-switch-crownstone.js
+++ crownstone-switch-crownstone.js
@@ -8,10 +8,12 @@ module.exports = function(RED) {
 
         // Input field values
         var crownstoneId = config.crownstone;
+        node.context().set("crownstoneId", crownstoneId);
         var crownstoneState = config.state;
+        var crownstonePercentage = config.percentage;
 
         // Retreive the cloud object from global context
-        var globalContext = this.context().global;
+        var globalContext = node.context().global;
         var cloud = globalContext.get("crownstoneCloud");
 
         /*
@@ -28,22 +30,27 @@ module.exports = function(RED) {
         
         // Input event
         node.on('input', function(msg) {
-            
-            // Use this for dimmable Crownstones
-            //crownstone.setSwitch(100);
 
             async function asyncFunciton() {
-
-                let cs = await cloud.crownstones();
-                console.log(cs);
-                msg.crownstones = cs; // Debug
-
                 let crownstone  = cloud.crownstone(crownstoneId);
-                if (crownstoneState){
-                    await crownstone.turnOn();
+
+                // Is the Crownstone dimmable
+                let crownstoneData = await crownstone.data();
+                let dimmable = crownstoneData.abilities.find(a => a.type === "dimming").enabled;
+
+                // Switch the crownstone
+                if(dimmable){
+                    //console.log("Dimmable: " + crownstonePercentage); // Debug
+                    await crownstone.setSwitch(crownstonePercentage);
                 }
                 else{
-                    await crownstone.turnOff();
+                    //console.log("Not dimmable: " + crownstoneState); // Debug
+                    if (crownstoneState){
+                        await crownstone.turnOn();
+                    }
+                    else{
+                        await crownstone.turnOff();
+                    }
                 }
 
                 node.send(msg);
@@ -62,10 +69,13 @@ module.exports = function(RED) {
     }
     RED.nodes.registerType("crownstone switch crownstone", CrownstoneSwitchCrownstone);
 
+    // This section is for the oneditprepare event in the browser to get a list of Crownstones.
     RED.httpAdmin.get("/crownstones/:id", function(req,res) {
         var node = RED.nodes.getNode(req.params.id); // This is a reference to the durrent deployed node in runtime. This does not work if the user just dragged the node on the workspace.
         var globalContext = node.context().global;
         var cloud = globalContext.get("crownstoneCloud"); // TODO: check if the global variable exists.
+
+        var selectedCrownstoneId = node.context().get("crownstoneId"); // Variables stored is context are not automaticaly updated like the other input values.
 
 
         async function asyncFunciton() {
@@ -73,8 +83,10 @@ module.exports = function(RED) {
 
             // Lambda expression to create a list of crownstone names and ids
             let crownstonesMapped = crownstones.map(cs => ({"name":cs.name, "id":cs.id, "dimming":cs.abilities.find(a => a.type === "dimming").enabled}));
-            console.log(crownstonesMapped); // Debug
-            res.json(crownstonesMapped);
+            
+            
+            //console.log(selectedCrownstoneId); // Debug
+            res.json({"crownstones":crownstonesMapped,"selectedCrownstoneId":selectedCrownstoneId});
         }
         asyncFunciton();
     });
